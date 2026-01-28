a8b4456da59ab78e5144dba7cd6742a96cd13ca0
abhinashdubey <abhinash.dubey@mogiio.com>
2026-01-28 14:57:38 +0530

    added config

--- rabc-ui/vite.config.js
+++ rabc-ui/vite.config.js
@@ -4,6 +4,16 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+    host: true,                     // allow external access
+    allowedHosts: [
+      "rbac-k8s.pve.mogiio.com"     // allow your domain explicitly
+    ],
+    proxy: {
+      "/api": {
+        target: "http://kube-api-backend:3001",
+        changeOrigin: true
+      }
+    }
   }
 });
