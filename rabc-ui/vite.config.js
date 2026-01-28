f5329994be56da84956c7c4a3ed209847401ac40
abhinashdubey <abhinash.dubey@mogiio.com>
2026-01-28 15:22:48 +0530

    added config

--- rabc-ui/vite.config.js
+++ rabc-ui/vite.config.js
@@ -1,19 +1,11 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  // Load env vars for the current mode
-  const env = loadEnv(mode, process.cwd(), "");
-
-  return {
-    plugins: [react()],
-    server: {
-      port: 3000,
-      host: true,
-      allowedHosts: env.VITE_ALLOWED_HOST
-        ? [env.VITE_ALLOWED_HOST]
-        : [],
-
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: true,
+    allowedHosts: "all"
+  }
 });
